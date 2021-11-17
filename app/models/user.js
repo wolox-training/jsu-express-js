017865838e5acc7c29aa3c3e3037da0139d09079
Howlz <juan.jose.suarez@accenture.com>
2021-11-17 17:00:01 -0500

    create user model and finished migration

---
+++ app/models/user.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      timestamps: true,
+      tableName: 'users',
+      underscored: true
+    }
+  );
+
+  return User;
+};
