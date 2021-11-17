ac079d5a79d17ffa1d8b7d361ad3cf5ba7041b34
Howlz <juan.jose.suarez@accenture.com>
2021-11-17 17:00:01 -0500

    create user model and finished migration
    weet service and sequelize modeling load

---
+++ app/services/weet.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const errors = require('../errors');
+const { apiGeeks } = require('../../config').common.api;
+
+
+exports.getGeekPhrase = async () => {
+  try {
+    const configRequest = {
+      method: 'get',
+      url: `${apiGeeks}?format=json`
+    };
+    const { data: response } = await axios(configRequest);
+    return response;
+  } catch (error) {
+    logger.errors(error)
+    throw errors.apiError('Error to get geeks phrase');
+  }
+};
